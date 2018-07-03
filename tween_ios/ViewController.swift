df441f1421dd6d2b658d417e9211c17d4efba569
Boeing737ng <ghlove0317@naver.com>
2018-07-03 16:06:21 +0900

    Basic camera & library action created

--- tween_ios/ViewController.swift
+++ tween_ios/ViewController.swift
@@ -7,9 +7,13 @@
 //
 
 import UIKit
+import MobileCoreServices
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
+    
+    var capturedImage: UIImage!
+    var isTakenFromCamera = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -17,19 +21,39 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 
     @IBAction func openCamera(_ sender: Any) {
         if UIImagePickerController.isSourceTypeAvailable(.camera){
+            isTakenFromCamera = true
             let pickerController = UIImagePickerController()
             pickerController.delegate = self;
             pickerController.sourceType = .camera
             present(pickerController, animated: true, completion: nil)
         }
     }
+    
     @IBAction func chooseFromGallery(_ sender: Any) {
         if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
+            isTakenFromCamera = false
             let pickerController = UIImagePickerController()
             pickerController.delegate = self;
             pickerController.sourceType = .photoLibrary
             present(pickerController, animated: true, completion: nil)
         }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let mediaType = info[UIImagePickerControllerMediaType] as! NSString
+        print(mediaType)
+        
+        if mediaType.isEqual(to: kUTTypeImage as NSString as String) {
+            capturedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+            if isTakenFromCamera {
+                UIImageWriteToSavedPhotosAlbum(capturedImage, self, nil, nil)
+            }
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
