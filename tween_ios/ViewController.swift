0565bfef7fa8bc9ff8e7ada1e28b8a33491c8005
Boeing737ng <ghlove0317@naver.com>
2018-07-03 12:49:39 +0900

    tween app initialized

---
+++ tween_ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tween_ios
+//
+//  Created by Kihyun Choi on 2018. 7. 3..
+//  Copyright © 2018년 sfo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
