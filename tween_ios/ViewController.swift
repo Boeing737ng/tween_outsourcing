1ae22de3419cd5c0178cd914f79445f0226865bd
Boeing737ng <ghlove0317@naver.com>
2018-07-03 15:08:58 +0900

    Camera & Library button action created

--- tween_ios/ViewController.swift
+++ tween_ios/ViewController.swift
@@ -8,13 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func openCamera(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            let pickerController = UIImagePickerController()
+            pickerController.delegate = self;
+            pickerController.sourceType = .camera
+            present(pickerController, animated: true, completion: nil)
+        }
+    }
+    @IBAction func chooseFromGallery(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
+            let pickerController = UIImagePickerController()
+            pickerController.delegate = self;
+            pickerController.sourceType = .photoLibrary
+            present(pickerController, animated: true, completion: nil)
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
