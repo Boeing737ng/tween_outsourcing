3e9192e6ab046fbe9ddb51a2a81a51fb8bd84ced
Boeing737ng <ghlove0317@naver.com>
2018-07-08 23:55:06 +0900

    feat: Firebase imported

--- tween_ios/AppDelegate.swift
+++ tween_ios/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Stays on Launch screen for 3 seconds
         Thread.sleep(forTimeInterval: 3.0)
+        FirebaseApp.configure()
         // Override point for customization after application launch.
         return true
     }
